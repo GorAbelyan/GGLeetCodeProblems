a2d468d42294ffe29c666a7302540bcff2f19567
GorAbelyan <gor.abelyan.97@gmail.com>
2021-11-30 20:05:10 +0400

    commit

---
+++ LeetCodeProblems/ArrayProblems.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class ArrayProblems
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            var result = new int[2 * nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = nums[i];
+                result[i + nums.Length] = nums[i];
+            }
+            return result;
+        }
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int result = 0;
+            for (int i = 0; i < operations.Length; i++)
+            {
+                if (operations[i].Contains('+'))
+                {
+                    result++;
+                }
+                else
+                {
+                    result--;
+                }
+            }
+            return result;
+        }
+        public int[] RunningSum(int[] nums)
+        {
+            var result = new int[nums.Length];
+            int previusValue = nums[0];
+            result[0] = previusValue;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                result[i + 1] = result[i] + nums[i + 1];
+            }
+            return result;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -6,8 +6,11 @@ namespace LeetCodeProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            var ArrayProblems = new ArrayProblems();
+            ArrayProblems.GetConcatenation(new int[2]);
+            ArrayProblems.FinalValueAfterOperations(null);
+            ArrayProblems.RunningSum(null);
+
         }
     }
 }
